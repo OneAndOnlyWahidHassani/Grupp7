4cdb074836faf425020b592481fee67833696a29
AlanahCoolman <alanah.emily.coleman@gmail.com>
2025-02-20 20:50:33 +0100

    Button for theme

--- src/main/java/LevelEditor/view/SetUp.java
+++ src/main/java/LevelEditor/view/SetUp.java
@@ -25,6 +25,9 @@ public class SetUp extends Pane {
     private Image returnResize;
     private AudioPlayer audioPlayer;
 
+    //Button
+    private Image RightButton;
+    private Image LeftButton;
 
     //Themes
     private Image cloud;
@@ -60,16 +63,22 @@ public class SetUp extends Pane {
      * Metod som länkar Image-objekten till png-filer
      */
     public void setupImages() {
+        titel = new Image("file:files/texts/lvl.png", 300, 80, false, true);
         chooseDimension = new Image("file:files/texts/ChooseDimension.png", 400, 300, false, false);
-        tenByTen = new Image("file:files/texts/10x10.png", 250, 30, false, false);
-        tenByTenResize = new Image("file:files/texts/10x10.png", 255, 33, false, false);
-        fourteen = new Image("file:files/texts/14x14.png", 250, 30, false, false);
-        fourteenResize = new Image("file:files/texts/14x14.png", 255, 33, false, false);
-        eighteen = new Image("file:files/texts/18x18.png", 250, 30, false, false);
-        eighteenResize = new Image("file:files/texts/18x18.png", 255, 33, false, false);
+        tenByTen = new Image("file:files/texts/10x10.png", 200, 30, false, false);
+        tenByTenResize = new Image("file:files/texts/10x10.png", 205, 33, false, false);
+        fourteen = new Image("file:files/texts/14x14.png", 200, 30, false, false);
+        fourteenResize = new Image("file:files/texts/14x14.png", 205, 33, false, false);
+        eighteen = new Image("file:files/texts/18x18.png", 200, 30, false, false);
+        eighteenResize = new Image("file:files/texts/18x18.png", 205, 33, false, false);
 
-        Theme = new Image("file:files/texts/Level1.png", 250, 30, false, false);
+        Theme = new Image("file:files/texts/theme/cloud.png", 250, 30, false, false);
         ThemeResize = new Image("file:files/texts/Level1.png", 255, 33, false, false);
+
+        //Button Choode
+        RightButton = new Image("file:files/texts/RB.png", 430, 300, false, false);
+        LeftButton = new Image("file:files/texts/LB.png", 430, 300, false, false);
+
 
         //Themes
         cloud = new Image("file:files/texts/Level1.png", 250, 30, false, false);
@@ -113,26 +122,31 @@ public class SetUp extends Pane {
      */
     public void addButtons() {
 
+        ImageView titelImageView = new ImageView(titel);
+        titelImageView.setStyle("fx-background-color: transparent;");
+        titelImageView.setTranslateX(250);
+        titelImageView.setTranslateY(50);
+
         ImageView dimensionView = new ImageView(chooseDimension);
         dimensionView.setStyle("fx-background-color: transparent;");
-        dimensionView.setTranslateX(350);
+        dimensionView.setTranslateX(200);
         dimensionView.setTranslateY(150);
 
 
         ImageView tenByTenView = new ImageView(tenByTen);
         tenByTenView.setStyle("fx-background-color: transparent;");
-        tenByTenView.setTranslateX(400);
+        tenByTenView.setTranslateX(275);
         tenByTenView.setTranslateY(200);
         tenByTenView.toFront();
         tenByTenView.setPickOnBounds(true);
         tenByTenView.setOnMouseEntered(e -> {
             tenByTenView.setImage(tenByTenResize);
-            tenByTenView.setTranslateX(400);
+            tenByTenView.setTranslateX(273);
             tenByTenView.setTranslateY(198);
         });
         tenByTenView.setOnMouseExited(e -> {
             tenByTenView.setImage(tenByTen);
-            tenByTenView.setTranslateX(400);
+            tenByTenView.setTranslateX(275);
             tenByTenView.setTranslateY(200);
         });
         tenByTenView.setOnMouseClicked(e -> {
@@ -146,18 +160,18 @@ public class SetUp extends Pane {
 
         ImageView fourteenView = new ImageView(fourteen);
         fourteenView.setStyle("fx-background-color: transparent;");
-        fourteenView.setTranslateX(400);
+        fourteenView.setTranslateX(275);
         fourteenView.setTranslateY(250);
         fourteenView.toFront();
         fourteenView.setPickOnBounds(true);
         fourteenView.setOnMouseEntered(e -> {
             fourteenView.setImage(fourteenResize);
-            fourteenView.setTranslateX(400);
+            fourteenView.setTranslateX(273);
             fourteenView.setTranslateY(248);
         });
         fourteenView.setOnMouseExited(e -> {
             fourteenView.setImage(fourteen);
-            fourteenView.setTranslateX(400);
+            fourteenView.setTranslateX(275);
             fourteenView.setTranslateY(250);
         });
         fourteenView.setOnMouseClicked(e -> {
@@ -171,18 +185,18 @@ public class SetUp extends Pane {
 
         ImageView eighteenView = new ImageView(eighteen);
         eighteenView.setStyle("fx-background-color: transparent;");
-        eighteenView.setTranslateX(400);
+        eighteenView.setTranslateX(275);
         eighteenView.setTranslateY(300);
         eighteenView.toFront();
         eighteenView.setPickOnBounds(true);
         eighteenView.setOnMouseEntered(e -> {
             eighteenView.setImage(eighteenResize);
-            eighteenView.setTranslateX(400);
+            eighteenView.setTranslateX(273);
             eighteenView.setTranslateY(298);
         });
         eighteenView.setOnMouseExited(e -> {
             eighteenView.setImage(eighteen);
-            eighteenView.setTranslateX(400);
+            eighteenView.setTranslateX(275);
             eighteenView.setTranslateY(300);
         });
         eighteenView.setOnMouseClicked(e -> {
@@ -194,24 +208,24 @@ public class SetUp extends Pane {
             }
         });
 
-        //CLOUD
-        ImageView cloudView = new ImageView(cloud);
-        cloudView.setStyle("fx-background-color: transparent;");
-        cloudView.setTranslateX(100);
-        cloudView.setTranslateY(200);
-        cloudView.toFront();
-        cloudView.setPickOnBounds(true);
-        cloudView.setOnMouseEntered(e -> {
-            cloudView.setImage(cloudResize);
-            cloudView.setTranslateX(100);
-            cloudView.setTranslateY(200);
+
+        ImageView RButtonView = new ImageView(RightButton);
+        RButtonView.setStyle("fx-background-color: transparent;");
+        RButtonView.setTranslateX(300);
+        RButtonView.setTranslateY(300);
+        RButtonView.toFront();
+        RButtonView.setPickOnBounds(true);
+        RButtonView.setOnMouseEntered(e -> {
+            RButtonView.setImage(RightButton);
+            RButtonView.setTranslateX(300);
+            RButtonView.setTranslateY(300);
         });
-        cloudView.setOnMouseExited(e -> {
-            cloudView.setImage(cloud);
-            cloudView.setTranslateX(100);
-            cloudView.setTranslateY(200);
+        RButtonView.setOnMouseExited(e -> {
+            RButtonView.setImage(RightButton);
+            RButtonView.setTranslateX(300);
+            RButtonView.setTranslateY(300);
         });
-        cloudView.setOnMouseClicked(e -> {
+        RButtonView.setOnMouseClicked(e -> {
             try {
                 mainProgram.changeToRandomize(28);
                 audioPlayer.playButtonSound();
@@ -221,23 +235,77 @@ public class SetUp extends Pane {
         });
 
         //Dessert
-        ImageView dessertView = new ImageView(desert);
-        dessertView.setStyle("fx-background-color: transparent;");
-        dessertView.setTranslateX(100);
-        dessertView.setTranslateY(250);
-        dessertView.toFront();
-        dessertView.setPickOnBounds(true);
-        dessertView.setOnMouseEntered(e -> {
-            dessertView.setImage(desertResize);
-            dessertView.setTranslateX(100);
-            dessertView.setTranslateY(250);
+        ImageView LButtonView = new ImageView(LeftButton);
+        LButtonView.setStyle("fx-background-color: transparent;");
+        LButtonView.setTranslateX(10);
+        LButtonView.setTranslateY(300);
+        LButtonView.toFront();
+        LButtonView.setPickOnBounds(true);
+        LButtonView.setOnMouseEntered(e -> {
+            LButtonView.setImage(LeftButton);
+            LButtonView.setTranslateX(10);
+            LButtonView.setTranslateY(300);
         });
-        dessertView.setOnMouseExited(e -> {
-            dessertView.setImage(desert);
-            dessertView.setTranslateX(100);
-            dessertView.setTranslateY(250);
+        LButtonView.setOnMouseExited(e -> {
+            LButtonView.setImage(LeftButton);
+            LButtonView.setTranslateX(10);
+            LButtonView.setTranslateY(300);
         });
-        dessertView.setOnMouseClicked(e -> {
+        LButtonView.setOnMouseClicked(e -> {
+            try {
+                mainProgram.changeToRandomize(28);
+                audioPlayer.playButtonSound();
+            } catch (FileNotFoundException fileNotFoundException) {
+                fileNotFoundException.printStackTrace();
+            }
+        });
+
+
+
+        //CLOUD
+      /*  ImageView RButtonView = new ImageView(cloud);
+        RButtonView.setStyle("fx-background-color: transparent;");
+        RButtonView.setTranslateX(100);
+        RButtonView.setTranslateY(200);
+        RButtonView.toFront();
+        RButtonView.setPickOnBounds(true);
+        RButtonView.setOnMouseEntered(e -> {
+            RButtonView.setImage(cloudResize);
+            RButtonView.setTranslateX(100);
+            RButtonView.setTranslateY(200);
+        });
+        RButtonView.setOnMouseExited(e -> {
+            RButtonView.setImage(cloud);
+            RButtonView.setTranslateX(100);
+            RButtonView.setTranslateY(200);
+        });
+        RButtonView.setOnMouseClicked(e -> {
+            try {
+                mainProgram.changeToRandomize(28);
+                audioPlayer.playButtonSound();
+            } catch (FileNotFoundException fileNotFoundException) {
+                fileNotFoundException.printStackTrace();
+            }
+        });
+
+        //Dessert
+        ImageView LButtonView = new ImageView(desert);
+        LButtonView.setStyle("fx-background-color: transparent;");
+        LButtonView.setTranslateX(100);
+        LButtonView.setTranslateY(250);
+        LButtonView.toFront();
+        LButtonView.setPickOnBounds(true);
+        LButtonView.setOnMouseEntered(e -> {
+            LButtonView.setImage(desertResize);
+            LButtonView.setTranslateX(100);
+            LButtonView.setTranslateY(250);
+        });
+        LButtonView.setOnMouseExited(e -> {
+            LButtonView.setImage(desert);
+            LButtonView.setTranslateX(100);
+            LButtonView.setTranslateY(250);
+        });
+        LButtonView.setOnMouseClicked(e -> {
             try {
                 mainProgram.changeToRandomize(28);
                 audioPlayer.playButtonSound();
@@ -352,7 +420,7 @@ public class SetUp extends Pane {
             } catch (FileNotFoundException fileNotFoundException) {
                 fileNotFoundException.printStackTrace();
             }
-        });
+        }); */
 
 
         ImageView returnView = new ImageView(returnImage);
@@ -380,6 +448,7 @@ public class SetUp extends Pane {
 
 
 
-        getChildren().addAll(dimensionView, tenByTenView, fourteenView, eighteenView, cloudView, dessertView, forestView, lavaView, spaceView, undergroundView,  returnView);
+        getChildren().addAll(titelImageView, dimensionView, tenByTenView, fourteenView, eighteenView,RButtonView, LButtonView, returnView);
+        //RButtonView, LButtonView, forestView, lavaView, spaceView, undergroundView,
     }
 }
